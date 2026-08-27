481eacbe0473a5bb41defff8baa9a640e21dfa39
SamuelLacerdx <samuell.rodri13@gmail.com>
2026-08-26 21:14:19 -0300

    imagens adicionadas no banner

--- src/components/Banner.jsx
+++ src/components/Banner.jsx
@@ -1,11 +1,37 @@
-import imgBanner from "../assets/gtaBanner.jpeg";
+import { useState, useEffect } from "react";
+import imgBanner1 from "../assets/PS5Banner1.jpeg";
+import imgBanner2 from "../assets/PS5Banner2.jpeg";
+import imgBanner3 from "../assets/PS5Banner3.jpeg";
 
-function Banner() {
+const banners = [imgBanner1, imgBanner2, imgBanner3];
+
+function Carousel() {
+  const [current, setCurrent] = useState(0);
+
+  useEffect(() => {
+    const id = setInterval(() => {
+      setCurrent((prev) => (prev + 1) % banners.length);
+    }, 3000);
+    return () => clearInterval(id);
+  }, []);
+
   return (
-    <section>
-      <img src={imgBanner} alt="Banner PS5" className="w-full object-contain" />
+    <section className="object-cover w-full h-200 ">
+      <div
+        className="flex h-full transition-transform duration-500 ease-out"
+        style={{ transform: `translateX(-${current * 100}%)` }}
+      >
+        {banners.map((banner, index) => (
+          <img
+            key={index}
+            src={banner}
+            alt={`Banner PS5 ${index + 1}`}
+            className="w-full h-full flex-shrink-0 object-cover"
+          />
+        ))}
+      </div>
     </section>
   );
 }
 
-export default Banner;
+export default Carousel;
--- src/components/Navegacao.jsx
+++ src/components/Navegacao.jsx
@@ -1,5 +1,6 @@
 import { Link } from "react-router-dom";
 import { useState, useEffect } from "react";
+import Logo from "../assets/LogoSite.png";
 
 function Navegacao() {
   const [quantidade, setQuantidade] = useState(0);
@@ -10,26 +11,53 @@ function Navegacao() {
     }
 
     window.addEventListener("adicionarAoCarrinho", handleAdicionar);
-    return () => window.removeEventListener("adicionarAoCarrinho", handleAdicionar);
+    return () =>
+      window.removeEventListener("adicionarAoCarrinho", handleAdicionar);
   }, []);
 
   return (
     <header className="grid grid-cols-3 items-center p-5 bg-blue-400">
       <nav className="flex gap-5 text-white">
-        <Link to="/" >Home</Link >
+        <Link to="/">Home</Link>
         <Link to="/Game">Video-Game</Link>
         <Link to="/Jogos">Jogos</Link>
         <Link to="/Acessorios">Acessorios</Link>
       </nav>
 
-      <h1 className="text-3xl text-center text-white">Pichau</h1>
-
-      <div className="flex gap-3 justify-end items-center">
-        <img src="" alt="RedeSocial" />
-        <img src="" alt="RedeSocial" />
-
+      <img
+        src={Logo}
+        alt="Pichau do Play"
+        className="h-20 w-auto mx-auto object-contain"
+      />
+      <div className="flex gap-7 justify-end items-center">
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          height="30px"
+          viewBox="0 -960 960 960"
+          width="30px"
+          fill="#e3e3e3"
+        >
+          <path d="M784-120 532-372q-30 24-69 38t-83 14q-109 0-184.5-75.5T120-580q0-109 75.5-184.5T380-840q109 0 184.5 75.5T640-580q0 44-14 83t-38 69l252 252-56 56ZM380-400q75 0 127.5-52.5T560-580q0-75-52.5-127.5T380-760q-75 0-127.5 52.5T200-580q0 75 52.5 127.5T380-400Z" />
+        </svg>
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          height="24px"
+          viewBox="0 -960 960 960"
+          width="24px"
+          fill="#e3e3e3"
+        >
+          <path d="M234-276q51-39 114-61.5T480-360q69 0 132 22.5T726-276q35-41 54.5-93T800-480q0-133-93.5-226.5T480-800q-133 0-226.5 93.5T160-480q0 59 19.5 111t54.5 93Zm146.5-204.5Q340-521 340-580t40.5-99.5Q421-720 480-720t99.5 40.5Q620-639 620-580t-40.5 99.5Q539-440 480-440t-99.5-40.5ZM480-80q-83 0-156-31.5T197-197q-54-54-85.5-127T80-480q0-83 31.5-156T197-763q54-54 127-85.5T480-880q83 0 156 31.5T763-763q54 54 85.5 127T880-480q0 83-31.5 156T763-197q-54 54-127 85.5T480-80Zm100-95.5q47-15.5 86-44.5-39-29-86-44.5T480-280q-53 0-100 15.5T294-220q39 29 86 44.5T480-160q53 0 100-15.5ZM523-537q17-17 17-43t-17-43q-17-17-43-17t-43 17q-17 17-17 43t17 43q17 17 43 17t43-17Zm-43-43Zm0 360Z" />
+        </svg>
         <div className="relative text-2xl">
-          🛒
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            height="24px"
+            viewBox="0 -960 960 960"
+            width="24px"
+            fill="#e3e3e3"
+          >
+            <path d="M223.5-103.5Q200-127 200-160t23.5-56.5Q247-240 280-240t56.5 23.5Q360-193 360-160t-23.5 56.5Q313-80 280-80t-56.5-23.5Zm400 0Q600-127 600-160t23.5-56.5Q647-240 680-240t56.5 23.5Q760-193 760-160t-23.5 56.5Q713-80 680-80t-56.5-23.5ZM246-720l96 200h280l110-200H246Zm-38-80h590q23 0 35 20.5t1 41.5L692-482q-11 20-29.5 31T622-440H324l-44 80h480v80H280q-45 0-68-39.5t-2-78.5l54-98-144-304H40v-80h130l38 80Zm134 280h280-280Z" />
+          </svg>
           <span className="absolute -top-2 -right-3 bg-red-600 text-white text-xs font-bold rounded-full w-5 h-5 flex items-center justify-center">
             {quantidade}
           </span>
